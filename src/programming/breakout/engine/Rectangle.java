7260de78c9b2c2a84fdce63ebfe4d4e0ffa8b0d9
blu7 <felix.vdheide@web.de>
2016-02-24 17:06:33 +0100

    Rectangles

---
+++ src/programming/breakout/engine/Rectangle.java
@@ -0,0 +1,48 @@
+package programming.breakout.engine;
+
+public class Rectangle extends Entity {
+	
+	/**
+	 * instance variables
+	 */
+	private double height = 0;
+	private double width = 0;
+
+	/**
+	 * primary constructor
+	 */
+	Rectangle(Vector2D position, double height, double width) {
+		this.setPosition(position);
+		this.height = height;
+		this.width = width;
+	}
+
+	/**
+	 * Returns the height of the Rectangle
+	 */
+	public double getHeight() {
+		return height;
+	}
+	
+	/**
+	 * Sets the height of the Rectangle
+	 */
+	void setHeight(double height) {
+		this.height = height;
+	}
+	
+	/**
+	 * Returns the width of the Rectangle
+	 */
+	public double getWidth() {
+		return width;
+	}
+	
+	/**
+	 * Sets the width of the Rectangle
+	 */
+	void setWidth(double width) {
+		this.width = width;
+	}
+
+}
