eb9be30938686d46ee4bd291d3757ef17e2b00f8
Jan Path <mail@jpath.de>
2016-02-24 18:00:43 +0100

    Add ball class

---
+++ src/programming/breakout/engine/Ball.java
@@ -0,0 +1,22 @@
+package programming.breakout.engine;
+
+/**
+ * A ball
+ */
+public class Ball extends Entity {
+	private double radius;
+
+	/**
+	 * @return the radius
+	 */
+	public double getRadius() {
+		return radius;
+	}
+
+	/**
+	 * @param radius the radius to set
+	 */
+	void setRadius(double radius) {
+		this.radius = radius;
+	}
+}
