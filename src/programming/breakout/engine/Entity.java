cb1e282300b427fb69beced6826407fae1455693
Jan Path <mail@jpath.de>
2016-02-24 18:05:25 +0100

    Add getX and getY to entity for convenience

--- src/programming/breakout/engine/Entity.java
+++ src/programming/breakout/engine/Entity.java
@@ -7,6 +7,8 @@ public abstract class Entity {
 	private Vector2D velocity = new Vector2D(0, 0);
 	private Vector2D position = new Vector2D(0, 0);
 
+	private boolean visible;
+
 	/**
 	 * @return the acceleration
 	 */
@@ -47,5 +49,33 @@ public abstract class Entity {
 	 */
 	void setPosition(Vector2D position) {
 		this.position = position;
+	}
+
+	/**
+	 * @return the visible
+	 */
+	public boolean isVisible() {
+		return visible;
+	}
+
+	/**
+	 * @param visible the visible to set
+	 */
+	void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	/**
+	 * @return the x coordinate of the object
+	 */
+	public double getX() {
+		return position.getX0();
+	}
+
+	/**
+	 * @return the y coordinate of the object
+	 */
+	public double getY() {
+		return position.getX1();
 	}
 }
